b15c495974c0225c5f4744a6941dc1fc53850b12
Manshu <manshus389@gmail.com>
2025-03-13 21:26:19 +0530

    feat: added patientResponse code in PatientService

--- patient-service/src/main/java/com/pm/patientservice/service/PatientService.java
+++ patient-service/src/main/java/com/pm/patientservice/service/PatientService.java
@@ -1,13 +1,13 @@
 package com.pm.patientservice.service;
 
 
+import com.pm.patientservice.dto.PatientRequestDTO;
 import com.pm.patientservice.dto.PatientResponseDTO;
 import com.pm.patientservice.mapper.PatientMapper;
 import com.pm.patientservice.model.Patient;
 import com.pm.patientservice.repository.PatientRepository;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -30,5 +30,11 @@ public class PatientService {
 
         //List<PatientResponseDTO> patientResponseDTOs = patients.stream()
                 //.map(PatientMapper::toDTO).toList(); it was replaced by inline variable
+    }
+
+    //why used patientRequestDTO
+    public PatientResponseDTO createPatient(PatientRequestDTO patientRequestDTO) {
+        Patient newPatient = patientRepository.save(patientRequestDTO);
+        //need to use mapper to convert patientRequestDTO to patient Model entity because above is giving error
     }
 }
