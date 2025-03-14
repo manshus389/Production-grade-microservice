e4dc71b4f177179642159b8d0f503d85db5dc7d0
Manshu <manshus389@gmail.com>
2025-03-14 13:43:36 +0530

    chore: added logger in GlobalExceptionHandler

--- patient-service/src/main/java/com/pm/patientservice/exception/GlobalExceptionHandler.java
+++ patient-service/src/main/java/com/pm/patientservice/exception/GlobalExceptionHandler.java
@@ -1,5 +1,7 @@
 package com.pm.patientservice.exception;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -11,6 +13,9 @@ import java.util.Map;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
+
+    //added to debug the issue and added after exception handling
+    private static final Logger log = LoggerFactory.getLogger(GlobalExceptionHandler.class);
 
     //below is used in validation error that get triggered in requestDTO in the request itself
     @ExceptionHandler(MethodArgumentNotValidException.class)
@@ -25,6 +30,9 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(EmailAlreadyExistsException.class)
     public ResponseEntity<Map<String, String>> handleEmailAlreadyExistsException(EmailAlreadyExistsException ex) {
+
+        log.warn("Email address already exists {}",ex.getMessage());
+        //above line was added afterwards
         Map<String, String> errors = new HashMap<>();
         errors.put("message", "Email address already exists");
         return ResponseEntity.badRequest().body(errors);
