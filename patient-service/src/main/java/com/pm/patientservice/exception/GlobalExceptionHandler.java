955eb637193e6f0591ac1ce3caaf771563f76876
Manshu <manshus389@gmail.com>
2025-03-14 13:13:32 +0530

    feat: added GlobalExceptionHandler for controller

---
+++ patient-service/src/main/java/com/pm/patientservice/exception/GlobalExceptionHandler.java
@@ -0,0 +1,25 @@
+package com.pm.patientservice.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    //below is used in validation error that get triggered in requestDTO in the request itself
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidationException(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+
+        ex.getBindingResult().getFieldErrors().forEach(
+                error -> errors.put(error.getField(), error.getDefaultMessage()));
+
+        return ResponseEntity.badRequest().body(errors);
+    }
+}
