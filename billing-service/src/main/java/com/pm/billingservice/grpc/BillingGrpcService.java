478e5ddf9db600663042f74fb0072326b0e16d12
Manshu <manshus389@gmail.com>
2025-03-16 18:49:11 +0530

    feat: dockerised create-billing service and changed pom.xml for it

---
+++ billing-service/src/main/java/com/pm/billingservice/grpc/BillingGrpcService.java
@@ -0,0 +1,32 @@
+package com.pm.billingservice.grpc;
+
+import billing.BillingRequest;
+import billing.BillingResponse;
+import billing.BillingServiceGrpc.BillingServiceImplBase;
+import io.grpc.stub.StreamObserver;
+import net.devh.boot.grpc.server.service.GrpcService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@GrpcService
+public class BillingGrpcService extends BillingServiceImplBase {
+    private static final Logger log = LoggerFactory.getLogger(BillingGrpcService.class);
+
+    @Override
+    public void createBillingAccount(BillingRequest billingrequest, StreamObserver<BillingResponse> responseObserver) {
+        //streamObserver in gRPC is very powerfull and due to this bidirection communiction is possible
+
+        log.info("createBillingAccount request received {}", billingrequest.toString());
+
+        //Business logic eg save to database,perform calculation etc
+
+        BillingResponse response = BillingResponse.newBuilder()
+                .setAccountId("12345")
+                .setStatus("ACTIVE")
+                .build();
+
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
+    }
+
+}
