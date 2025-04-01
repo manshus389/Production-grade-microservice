0daada1e464d0f091227bae494d93fa4c992f0e2
Manshu <manshus389@gmail.com>
2025-04-01 21:18:21 +0530

    feat: added shell script for localstack and added api-gateway yml file for prod

---
+++ infrastructure/localstack-deploy.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -e #stops the script if any commands fails
+
+aws --endpoint-url=http://localhost:4566 cloudformation deploy \
+    --stack-name patient-management \
+    --template-file "./cdk.out/localstack.template.json"
+
+aws --endpoint-url=http://localhost:4566 elbv2 describe-load-balancers \
+    --query "LoadBalancers[0].DNSName" --output text
