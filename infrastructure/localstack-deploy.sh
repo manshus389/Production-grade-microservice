dbcb0e6ec81cfc92b0786acfc16133fd5cc97c20
Manshu <manshus389@gmail.com>
2025-04-01 21:37:09 +0530

    chore: added alb host in place of domain name and added re-built command in shell script

--- infrastructure/localstack-deploy.sh
+++ infrastructure/localstack-deploy.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -e #stops the script if any commands fails
 
+#if you want to re-deploy the stack add the below command in script
+#aws --endpoint-url=http://localhost:4566 cloudformation delete-stack \
+#    --stack-name patient-management
+
 aws --endpoint-url=http://localhost:4566 cloudformation deploy \
     --stack-name patient-management \
     --template-file "./cdk.out/localstack.template.json"
